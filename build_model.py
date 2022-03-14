f62f6aa88d3bfcdc95b089de97242fb74c82ed81
jamah97 <88891435+jamah97@users.noreply.github.com>
2022-03-14 15:36:58 -0500

    Update build_model.py

--- build_model.py
+++ build_model.py
@@ -59,13 +59,13 @@ test_datagen = ImageDataGenerator( rescale = 1.0/255. )
 train_generator = train_datagen.flow_from_directory(train_dir,
                                                     batch_size = 20,
                                                     class_mode = 'categorical',
-                                                    target_size = (224, 224))
+                                                    target_size = (299, 299))
 
 # Flow validation images in batches of 20 using test_datagen generator
 validation_generator = test_datagen.flow_from_directory( test_dir,
                                                         batch_size = 20,
                                                         class_mode = 'categorical',
-                                                        target_size = (224, 224))
+                                                        target_size = (299, 299))
 
 
 
